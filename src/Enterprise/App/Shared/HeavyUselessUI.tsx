b22ecc3dd6bc3fb451e79f1a31b62f559265e6b0
Andrew <yueh.andrew@gmail.com>
2024-06-02 00:36:38 -0700

    better rep

--- src/Enterprise/App/Shared/HeavyUselessUI.tsx
+++ src/Enterprise/App/Shared/HeavyUselessUI.tsx
@@ -2,7 +2,7 @@ import { useSelector } from "src/Enterprise/store";
 
 const HeavyUselessUI = () => {
   const slowdown = useSelector((state) => state.performance.slowdown)
-  const iterations = slowdown * 200
+  const iterations = slowdown * 400
 
   for (let i = 0; i < slowdown * 10000; i++) {
     JSON.parse(JSON.stringify({}))
