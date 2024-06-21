b4ccd89c4b5bdf089c3e6153752950fb28b7aac6
Andrew <yueh.andrew@gmail.com>
2024-06-21 01:18:48 -0700

    config

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": [
+        "react-app"
+    ],
+    "plugins": [
+        "eslint-plugin-react-compiler"
+    ],
+    "rules": {
+        "react-compiler/react-compiler": "error"
+    }
+}
